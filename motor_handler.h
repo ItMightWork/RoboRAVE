d2968bc0525fc5b9ef39c2e1a7303844756992ba
Tomáš Rada <schaddy12@gmail.com>
2016-02-18 21:22:15 +0100

    Motor control library
    Controling motors made easier

--- motor_handler.h
+++ motor_handler.h
@@ -9,39 +9,108 @@
 	//pwmPin = 19 - left
 	//pwmPin = 33 - right
 
-int motorMoveForw (int strengthPerc, int motorNum) {
+int motorMove (int strengthPerc, int motorNum) {
 
 	wiringPiSetup();
 	int pwmPin;
 	int motorSpeed = (int) (10.24*strengthPerc);
 	
-	while(1) {	
-		////LEFT MOTOR////
+		////FORWARD////
 
-		if (motorNum = 1) {
+		if (motorSpeed > 0) {
 			
-			pwmPin = 19; 
+			////LEFT MOTOR////
+
+			if (motorNum = 1) {
+			
+				pwmPin = 19; 
 					
-			pinMode(PHASE_PINL, OUTPUT);
-			digitalWrite(PHASE_PINL, LOW);  //Phase 
+				pinMode(PHASE_PINL, OUTPUT);
+				digitalWrite(PHASE_PINL, LOW);  //Phase 
 
-			pinMode(pmwPin, PWM_OUTPUT);
-			pwmWrite(pwmPin, motorSpeed);   //Enable
+				pinMode(pmwPin, PWM_OUTPUT);
+				pwmWrite(pwmPin, motorSpeed);   //Enable
+			}
+
+			///RIGHT MOTOR////
+
+			else if (motorNum = 2) {
+
+				pwmPin = 33;
+
+				pinMode(PHASE_PINR, OUTPUT);
+				digitalWrite(PHASE_PINR, LOW);	//Phase
+
+				pinMode(pwmPin, PWM_OUTPUT);
+				pwmWrite(pwmPinm, motorSpeed); //Enable
+			}
+		}
+	
+		////REVERSE////
+		
+		else if (motorSpeed < 0) {
+
+			motorSpeed = abs(motorSpeed);
+			
+			////LEFT MOTOR////
+			
+			if (motorNum = 1) {
+
+				pwmPin = 19; 
+					
+				pinMode(PHASE_PINL, OUTPUT);
+				digitalWrite(PHASE_PINL, HIGH);  //Phase 
+
+				pinMode(pmwPin, PWM_OUTPUT);
+				pwmWrite(pwmPin, motorSpeed);   //Enable
+			}
+
+			////RIGHT MOTOR////
+
+			else if (motorNum = 2) {
+
+				pwmPin = 33;
+
+				pinMode(PHASE_PINR, OUTPUT);
+				digitalWrite(PHASE_PINR, HIGH);	//Phase
+
+				pinMode(pwmPin, PWM_OUTPUT);
+				pwmWrite(pwmPinm, motorSpeed); //Enable
+			}
+
 		}
 
-		///RIGHT MOTOR////
+		////BRAKE////
+		
+		else {
 
-		else if (motorNum = 2) {
+			////LEFT MOTOR////
 
-			pwmPin = 33;
+			if (motorNum = 1) {
 
-			pinMode(PHASE_PINR, OUTPUT);
-			digitalWrite(PHASE_PINR, LOW);	//Phase
+				pwmPin = 19; 
+					
+				pinMode(PHASE_PINL, OUTPUT);
+				digitalWrite(PHASE_PINL, LOW);  //Phase - can be anything 
 
-			pinMode(pwmPin, PWM_OUTPUT);
-			pwmWrite(pwmPinm, motorSpeed); //Enable
+				pinMode(pmwPin, PWM_OUTPUT);
+				pwmWrite(pwmPin, motorSpeed);   //Enable  - must be 0
+			}
+
+			////RIGHT MOTOR////
+
+			else if (motorNum = 2) {
+
+				pwmPin = 33;
+
+				pinMode(PHASE_PINR, OUTPUT);
+				digitalWrite(PHASE_PINR, LOW);	//Phase - can be anything 
+
+				pinMode(pwmPin, PWM_OUTPUT);
+				pwmWrite(pwmPinm, motorSpeed); //Enable - must be 0
+			}
 		}
-	}
+
 	return 0;
 }
 
